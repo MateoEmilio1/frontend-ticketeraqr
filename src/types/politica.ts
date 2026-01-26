71ca2aec445e82218ebb00f66df66e40cbc56dfa
Miguel58000 <miguelrodriguezips36@gmail.com>
2026-01-26 01:46:24 -0300

    Frontend Politicas

---
+++ src/types/politica.ts
@@ -0,0 +1,10 @@
+export interface Politica {
+    fechaVigencia: string;
+    diasReembolso: number;
+    estado?: string;
+    vigente?: boolean;
+}
+
+export interface PoliticaFormData {
+    diasReembolso: number;
+}
